77c15b353a8ab587ac16a9a2956ef87bdb439f94
Neeyo <jakub.nycz99@gmail.com>
2025-01-13 14:51:29 +0100

    Add client credentials to allow postman to request tokens

---
+++ src/IdentityService/Services/CustomProfileService.cs
@@ -0,0 +1,35 @@
+using System.Security.Claims;
+using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Services;
+using IdentityModel;
+using IdentityService.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityService.Services;
+
+public class CustomProfileService(UserManager<ApplicationUser> userManager) : IProfileService
+{
+    public async Task GetProfileDataAsync(ProfileDataRequestContext context)
+    {
+        var user = await userManager.GetUserAsync(context.Subject);
+        if (user == null || user.UserName == null) return;
+
+        var existingClaims = await userManager.GetClaimsAsync(user);
+        var nameClaim = existingClaims.FirstOrDefault(x => x.Type == JwtClaimTypes.Name);
+
+        var claims = new List<Claim>
+        {
+            new Claim("username", user.UserName)
+        };
+
+        context.IssuedClaims.AddRange(claims);
+        
+        if (nameClaim != null)
+            context.IssuedClaims.Add(nameClaim);
+    }
+
+    public Task IsActiveAsync(IsActiveContext context)
+    {
+        return Task.CompletedTask;
+    }
+}
